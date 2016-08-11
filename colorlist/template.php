77dff03373cb2cbc673f9d27a2ae68076c0cdf57
Mathieu Etienne <thiousi@gmail.com>
2016-08-11 10:53:54 +0200

    Removed shorthand <?= - Fix #1

--- colorlist/template.php
+++ colorlist/template.php
@@ -1,5 +1,5 @@
 <ul class="input-list field-grid color-list-field cf">
 <?php foreach($field->options() as $key => $value): ?>
-<li><label><input type="radio" name="color" value="<?= $value ?>" <?php e($field->value() == $value, 'checked') ?>/><span style="background: <?= $value ?>"></label></li>
+<li><label><input type="radio" name="color" value="<?php echo $value ?>" <?php e($field->value() == $value, 'checked') ?>/><span style="background: <?= $value ?>"></label></li>
 <?php endforeach ?>
 </ul>
