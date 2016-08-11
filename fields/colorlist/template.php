0130ccb06491e808bcb23ec850e3284d29c8e5ce
Mathieu Etienne <thiousi@gmail.com>
2016-08-11 19:40:35 +0200

    Added unset option

--- fields/colorlist/template.php
+++ fields/colorlist/template.php
@@ -1,29 +1,31 @@
 <ul class="input-list field-grid color-list-field cf">
-<?php foreach($field->options() as $key => $value): ?>
-<li>
-  <label>
-  <input type="radio" name="<?php echo $field->name() ?>" value="<?php echo $value ?>" <?php  e($field->readonly, 'disabled="true"'); e($field->value() == $value | $field->default() == $value, 'checked')  ?>/>
-    <span style="background: <?php echo $value ?>">
-  </label>
-</li>
-<?php endforeach ?>
+  <?php foreach($field->options() as $key => $value): ?>
+  <li>
+    <label>
+    <input type="radio" name="<?php echo $field->name() ?>" value="<?php echo $value ?>" <?php  e($field->readonly, 'disabled="true"'); e($field->value() == $value | $field->default() == $value, 'checked')  ?>/>
+      <span style="background: <?php echo $value ?>">
+    </label>
+  </li>
+  <?php endforeach ?>
 </ul>
 
-    <script type="text/javascript">
-        var allRadios = document.getElementsByName('<?php echo $field->name(); ?>');
-        var booRadio;
-        var x = 0;
-        for(x = 0; x < allRadios.length; x++){
+<?php if($field->unset()):?>
+<script type="text/javascript">
+  var allRadios = document.getElementsByName('<?php echo $field->name(); ?>');
+  var booRadio;
+  var x = 0;
+  for(x = 0; x < allRadios.length; x++){
 
-            allRadios[x].onclick = function() {
-                if(booRadio == this){
-                    this.checked = false;
-                    booRadio = null;
-                    console.log('1');
-                }else{
-                    booRadio = this;
-                    console.log('2');
-                }
-            };
+    allRadios[x].onclick = function() {
+      if(booRadio == this){
+          this.checked = false;
+          booRadio = null;
+          console.log('1');
+        }else{
+          booRadio = this;
+          console.log('2');
         }
-    </script>
+    };
+  }
+</script>
+<?php endif ?>
