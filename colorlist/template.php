7e46f5dbcfc856d0bed440ba77d626a688146e9f
Mathieu Etienne <thiousi@gmail.com>
2016-08-11 12:52:24 +0200

    Added readonly, default, fixed name issue

--- colorlist/template.php
+++ colorlist/template.php
@@ -1,5 +1,10 @@
 <ul class="input-list field-grid color-list-field cf">
 <?php foreach($field->options() as $key => $value): ?>
-<li><label><input type="radio" name="color" value="<?php echo $value ?>" <?php e($field->value() == $value, 'checked') ?>/><span style="background: <?php echo $value ?>"></label></li>
+<li>
+  <label>
+  <input type="radio" name="<?php echo $field->name() ?>" value="<?php echo $value ?>" <?php  e($field->readonly, 'disabled="true"'); e($field->value() == $value | $field->default() == $value, 'checked')  ?>/>
+    <span style="background: <?php echo $value ?>">
+  </label>
+</li>
 <?php endforeach ?>
 </ul>
